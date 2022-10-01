53c19c6c38ac532c212468a86b4e3e292cb8b865
Paul Beah <paulbeah84@gmail.com>
2022-10-01 02:57:25 +0000

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(47, 121);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(430, 47);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(274, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 37);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(274, 500);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 46);
+            this.label2.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 28;
+            this.listBox1.Location = new System.Drawing.Point(1240, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(530, 851);
+            this.listBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(722, 203);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(722, 203);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 269);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(661, 439);
+            this.checkedListBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(47, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 41);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Groups";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(214, 734);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 91);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(166, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(226, 41);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "QR Code Input";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(166, 224);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 41);
+            this.label6.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(609, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(25, 59);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1770, 851);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,373 @@
+﻿using iTextSharp.text;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    { 
+        
+        string connetionString = null;
+            SqlConnection cnn;
+        static int VALIDATION_DELAY = 1500;
+        System.Threading.Timer timer = null;
+
+        public SqlDataReader dataReader;
+        public SqlDataReader dataReaders;
+            string dateTime = DateTime.Now.ToShortTimeString();
+       //public DataTable dt = new DataTable();
+
+        static string id;
+        static string name;
+        static string group;
+        static string groupname;
+        static string status;
+        static string TableNo;
+        static string regdate;
+        public Form1()
+        {
+            InitializeComponent();
+              connetionString = "Data Source=DESKTOP-LH3ETHA\\SQLEXPRESS;Initial Catalog=invitation;integrated Security=true";
+            cnn = new SqlConnection(connetionString);
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            //pictureBox3.Hide();
+
+            textBox1.Focus();
+            FillListBox();
+            
+
+
+        }
+
+        private void FillListBox()
+        {
+            try
+            {
+                cnn.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Guest where Status = 'Present' ", cnn);
+
+                dataReader = cmd.ExecuteReader();
+
+                listBox1.Items.Clear();
+
+                while (dataReader.Read())
+                {
+                    listBox1.Items.Add(dataReader.GetValue(1) +" - " + dataReader.GetValue(2)+" - " + dataReader.GetValue(3)+" - " + dataReader.GetValue(5));
+                }
+
+                cnn.Close();
+            }
+
+            catch (Exception ex)
+            {
+              
+            }
+        }
+      
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox origin = sender as TextBox;
+            if (!origin.ContainsFocus)
+                return;
+
+            DisposeTimer();
+            timer = new System.Threading.Timer(TimerElapsed, null, VALIDATION_DELAY, VALIDATION_DELAY);
+
+        }
+        private void TimerElapsed(Object obj)
+        {
+            CheckSyntaxAndReport();
+            DisposeTimer();
+        }
+
+        private void DisposeTimer()
+        {
+            if (timer != null)
+            {
+                timer.Dispose();
+                timer = null;
+            }
+        }
+
+        private void CheckSyntaxAndReport()
+        {
+            this.Invoke(new Action(() =>
+            {
+                FillListBox();
+                ClearCHECKBOXLIST();
+                DisablePics();
+                string input = textBox1.Text;
+                if (cnn.State == ConnectionState.Closed)
+                    {
+                        cnn.Open();
+
+                    }
+                //SqlCommand cmd = new SqlCommand("Select * from Guest where Groups = '" + input + "'  ", cnn);
+
+
+                using (SqlCommand cmds = new SqlCommand("select GuestName,id,status, groups, groupname, concat(GuestName, ' ----- ' ,Status, ' ----- ', RegDate) as StatusName from guest  where Groups = '" + input + "' ", cnn))
+                {
+                    cmds.CommandType = CommandType.Text;
+
+                    
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmds))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+
+                        ((ListBox)checkedListBox1).DataSource = dt;
+
+                        ((ListBox)checkedListBox1).DisplayMember = "StatusName";
+                        ((ListBox)checkedListBox1).ValueMember = "id";
+                       
+                       
+                    }
+                }
+            
+                //try
+                //{
+
+
+
+
+
+            //using (dataReader = cmd.ExecuteReader())
+            //{
+            //    if (dataReader.HasRows)
+            //    {
+
+            //        while (dataReader.Read())
+            //        {
+            //            id = dataReader.GetValue(0).ToString();
+            //            name = dataReader.GetValue(1).ToString();
+            //            group = dataReader.GetValue(2).ToString();
+            //            groupname = dataReader.GetValue(3).ToString();
+            //            status = dataReader.GetValue(4).ToString();
+            //            regdate = dataReader.GetValue(5).ToString();
+            //            TableNo = dataReader.GetValue(6).ToString();
+
+            //        }
+            //    }
+            //    else
+            //    {
+            //        //pictureBox3.Hide();
+            //        pictureBox1.Hide();
+            //        pictureBox2.Show();
+            //        ClearCHECKBOXLIST();
+            //    }
+            //}
+
+            //if (status == "Present")
+            //        {
+            //        //pictureBox3.Show();
+            //        label7.Text = "Table Occupied";
+            //            pictureBox1.Hide();
+            //            pictureBox2.Hide();
+                    
+
+            //        }
+
+                    //else if (group != "") {
+
+                   
+                    //using (SqlCommand cmds = new SqlCommand("Select distinct[GuestName],id,status from Guest where Groups = '" + group + "' and Status is NULL ", cnn))
+                    //{
+                    //    cmds.CommandType = CommandType.Text;
+                    //    using (SqlDataAdapter sda = new SqlDataAdapter(cmds))
+                    //    {
+                    //        DataTable dt = new DataTable();
+                    //        sda.Fill(dt);
+                           
+                    //        ((ListBox)checkedListBox1).DataSource = dt;
+
+                    //        ((ListBox)checkedListBox1).DisplayMember = "GuestName";
+                    //        ((ListBox)checkedListBox1).ValueMember = "id";
+                    //        label6.Text = "- " + groupname;
+                    //        label7.Text = TableNo;
+                    //    }
+                    //}
+                
+
+                //SqlCommand cmdss = new SqlCommand("Select * from Guest where Groups = '" + group + "' ", cnn);
+
+                //       using (dataReader = cmds.ExecuteReader())
+                //        {
+
+                        
+                //        while (dataReader.Read())
+                //        {//DataTable dtEmp = new DataTable();
+                //         // add column to datatable  
+
+                //            //dtEmp.Columns.Add("EmpID", typeof(int));
+                //            //dtEmp.Columns.Add("EmpName", typeof(string));
+                //            //dtEmp.Columns.Add("EmpCity", typeof(string));
+                         
+                //            checkedListBox1.Items.Add(dataReader.GetValue(1).ToString() + " "+ dataReader.GetValue(2).ToString());
+                    
+                //        }
+                //                }
+                    
+                //    else if(status == "")
+                //    {
+                //        SqlCommand cmds = new SqlCommand("Update Guest set RegDate = '" + dateTime + "', status = 'Present' where id = '" + input + "' ", cnn);
+                //        int result = (int)cmds.ExecuteNonQuery();
+                //        pictureBox2.Hide();
+                //        //pictureBox3.Hide();
+                //        pictureBox1.Show();
+                //        cnn.Close();
+
+                //        cmds.Dispose();
+
+                //    label7.Text = TableNo;
+                //    //MessageBox.Show(dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + " - " + dataReader.GetValue(2));
+
+                //}
+
+                //else
+                //{
+                //    //pictureBox3.Hide();
+                //    pictureBox1.Hide(); 
+                //    pictureBox2.Show();
+                    
+
+                //}
+
+
+                    ClearALL();
+
+
+
+
+
+                //}
+                //catch (Exception ex)
+                //{
+                //    MessageBox.Show(ex.Message);
+
+                //}
+
+
+
+            }
+
+            
+
+                //string s = textBox1.Text.ToUpper(); //Do everything on the UI thread itself
+                //label1.Text = s;
+
+                ));
+        }
+
+        public void DisablePics()
+        {
+            pictureBox1.Hide();
+            pictureBox2.Hide();
+            //pictureBox3.Hide();
+        }
+
+       public void ClearALL()
+        {
+            textBox1.Clear();
+            FillListBox();
+            
+            //id = "";
+            //name = "";
+            //status = "";
+            //group = "";
+            //groupname = "";
+            //regdate = "";
+        } 
+        
+        public void ClearCHECKBOXLIST()
+        {
+            ((ListBox)checkedListBox1).DataSource = null;
+
+            for(int U = 0; U < checkedListBox1.Items.Count; U++)
+            {
+                checkedListBox1.Items.RemoveAt(U);
+            }
+
+            label6.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+              
+                    
+                   if(checkedListBox1.GetItemChecked(i))
+                {
+                    string Selected = checkedListBox1.SelectedValue.ToString();
+
+                    
+                    SqlCommand cmd = new SqlCommand("Select * from guest where id = '" + Selected + "' ", cnn);
+                    using (dataReader = cmd.ExecuteReader())
+                    {
+                        if (dataReader.HasRows)
+                        {
+
+                            while (dataReader.Read())
+                            {
+                                id = dataReader.GetValue(0).ToString();
+                                name = dataReader.GetValue(1).ToString();
+                                group = dataReader.GetValue(2).ToString();
+                                groupname = dataReader.GetValue(3).ToString();
+                                status = dataReader.GetValue(4).ToString();
+                                regdate = dataReader.GetValue(5).ToString();
+                                TableNo = dataReader.GetValue(6).ToString();
+
+                            }
+                        }
+                    }
+                    cmd.Dispose();
+                    label6.Text = "- " + groupname;
+                    label7.Text = TableNo;
+                    if (status != "")
+                    {
+                        pictureBox1.Hide();
+                        pictureBox2.Show();
+                    }
+                    else
+                    {
+                        SqlCommand cmds = new SqlCommand("Update Guest set RegDate = '" + dateTime + "', status = 'Present' where id = '" + Selected + "' ", cnn);
+                        int result = (int)cmds.ExecuteNonQuery();
+                        pictureBox2.Hide();
+                        //pictureBox3.Hide();
+                        pictureBox1.Show();
+                        cnn.Close();
+                        ClearALL();
+                        ClearCHECKBOXLIST();
+                        cmds.Dispose();
+
+                    }
+
+                }
+
+                //strcheck += "" + checkedListBox1.Items[i].ToString() + ",";
+
+            }
+
+          
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+    
+   
+
